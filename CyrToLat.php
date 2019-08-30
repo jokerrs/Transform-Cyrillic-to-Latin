e53017e0afef209d8bb49715ce1df1e2d8e56f70
Nemanja Jeremic <nemanja@joker.rs>
2019-08-31 00:54:50 +0200

    reformed for php7.0 >>

--- CyrToLat.php
+++ CyrToLat.php
@@ -1,20 +1,21 @@
 <?php
 /**
- *********************************************
- * @author JokerCMS | Nemanja Jeremic        *
- * @license GNU                              *
- * @version 1                                *
- *********************************************
- * @param $string                            *
- * @param $type null                         *
- * @return mixed|string                      *
- *********************************************
+ * @author Jeremic Nemanja nemanja@joker.rs
+ * @license GNU
+ * @version 0.0.8
  */
 
-function CyrToLat($string, $type = NULL) {
+
+/**
+ * @param string $string
+ * @param string|NULL $type
+ * @return string
+ */
+function cyrToLat(string $string, string $type = NULL):string
+{
     // By default we just transform Cry To Latin
     $CyrToLat =  array(
-    	'љ' => 'lj', 'њ' => 'nj', 'е' => 'e', 'р' => 'r', 'т' => 't', 'з' => 'z', 'у' => 'u', 'и' => 'i', 'о' => 'o', 'п' => 'p',
+        'љ' => 'lj', 'њ' => 'nj', 'е' => 'e', 'р' => 'r', 'т' => 't', 'з' => 'z', 'у' => 'u', 'и' => 'i', 'о' => 'o', 'п' => 'p',
         'ш' => 'š', 'ђ' => 'đ', 'а' => 'a', 'с' => 's', 'д' => 'd', 'ф' => 'f', 'г' => 'g', 'х' => 'h', 'ј' => 'j', 'к' => 'k',
         'л' => 'l', 'ч' => 'č', 'ћ' => 'ć', 'ж' => 'ž', 'ѕ' => 's', 'џ' => 'dž', 'ц' => 'c', 'в' => 'v', 'б' => 'b', 'н' => 'n',
         'м' => 'm', 'Љ' => 'Lj', 'Њ' => 'Nj', 'Е' => 'E', 'Р' => 'R', 'Т' => 'T', 'З' => 'Z', 'У' => 'U', 'И' => 'I', 'О' => 'O',
@@ -22,43 +23,40 @@ function CyrToLat($string, $type = NULL) {
         'К' => 'K', 'Л' => 'L', 'Ч' => 'Č', 'Ћ' => 'Ć', 'Ж' => 'Ž', 'Ѕ' => 'S', 'Џ' => 'Dž', 'Ц' => 'C', 'В' => 'V', 'Б' => 'B',
         'Н' => 'N', 'М' => 'M');
     $string = strtr($string, $CyrToLat);
-
     // If we need short latin we will use type ShortLatin
-    if ( $type == 'ShortLatin' || $type == 'url'  || $type == 'email' ) {
+    if ( $type === 'ShortLatin' || $type === 'url' || $type === 'email' ) {
         $ShortLatin = array(
-        	'ž' => 'z', 'š' => 's', 'đ' => 'dj', 'ć' => 'c', 'č' => 'c', 'dž' => 'dz',
+            'ž' => 'z', 'š' => 's', 'đ' => 'dj', 'ć' => 'c', 'č' => 'c', 'dž' => 'dz',
             'Ž' => 'Z', 'Š' => 'S', 'Đ' => 'Dj', 'Ć' => 'C', 'Č' => 'C', 'Dž' => 'Dz');
         $string = strtr($string, $ShortLatin);
     }
+    // Check for email
+    if ( $type === 'email' ) {
+        $email = array('.' => '(dot)', '@' => '(at)');
+        $string = strtr($string, $email);
+    }
+    // Check for url
+    if ( $type === 'url' ) {
+        $url = array('.' => '-','@' => '-at-', '_' => '-', '('  => '-', ')'  => '-',);
+        $string = strtr($string, $url);
+    }
     // But if is type == 'url' we change also few more characters
-    if ( $type == 'url' || $type == 'email') {
-
-        if($type == 'url'){
-            $url = array('.' => '-','@' => 'at', '_' => '-');
-            $string = strtr($string, $url);
-        }
-        $string = mb_strtolower($string, "UTF-8");
+    if ( $type === 'url' || $type === 'email' ) {
         $CyrToLat = array(
-        	' ' => '-', ','  => '', '!'  => '', '?'  => '', '/'  => '',
+            ' ' => '-', ','  => '', '!'  => '', '?'  => '', '/'  => '',
             '"'  => '', "'"  => '', '='  => '-', '\\'  => '', '~'  => '-',
-            ';'  => '-', ':'  => '-', '('  => '-', ')'  => '-', '$'  => '-dollar-',
-            '%'  => '-percent-', '&'  => '-and-', '^'  => '-', '*'  => 'star', '#'  => '-hash-tag-',
+            ';'  => '-', ':'  => '-',  '$'  => '-dollar-', '%'  => '-percent-',
+            '&'  => '-and-', '^'  => '-', '*'  => 'star', '#'  => '-hash-tag-',
             '<'  => '-', '>'  => '-', '+'  => '-plus-', '`'  => '');
         $string = strtr($string, $CyrToLat);
-
-        // Check for email
-        if($type == 'email'){
-            $email = array('.' => '(dot)', '@' => '(at)');
-            $string = strtr($string, $email);
-        }
 
         // remove duplicate -
         $string = preg_replace('~-+~', '-', $string);
         // removing last -
-        $string = rtrim($string,"-");
+        $string = rtrim($string, '-');
         // removing first -
-        $string = ltrim($string, "-");
-
+        $string = ltrim($string, '-');
     }
+
     return $string;
 }
